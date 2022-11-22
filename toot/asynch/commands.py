9e42d7f5dde2ea4169181aab842b2465c437b22d
Ivan Habunek <ivan@habunek.com>
2022-11-22 08:17:54 +0100

    wip

--- toot/asynch/commands.py
+++ toot/asynch/commands.py
@@ -1,29 +1,53 @@
 import asyncio
 import click
+import logging
 import os
-import sys
 import random
+import sys
 
 from typing import List, Optional, Tuple
+from toot import __version__
 from toot.asynch import api
 from toot.output import print_out
 from toot.utils import EOF_KEY, editor_input, multiline_input
 
-# Add shorthand -h for invoking help
-CONTEXT = dict(help_option_names=['-h', '--help'])
+# Tweak the Click context
+# https://click.palletsprojects.com/en/8.1.x/api/#context
+CONTEXT = dict(
+    # Enable using environment variables to specify options
+    auto_envvar_prefix='TOOT',
+    # Add shorthand -h for invoking help
+    help_option_names=['-h', '--help'],
+    # Give help some more room (default is 80)
+    max_content_width=100,
+    # Always show default values for options
+    show_default=True,
+)
+
+
+def validate_language(ctx, param, value: str) -> str:
+    if len(value) != 3:
+        raise click.BadParameter(
+            "Expected a 3 letter abbreviation according to ISO 639-2 standard."
+        )
+
+    return value
 
 
 @click.group(context_settings=CONTEXT)
-@click.option('--debug/--no-debug', default=False)
-@click.option('--color/--no-color', default=sys.stdout.isatty())
-def cli(debug: bool, color: bool):
-    click.echo(f"Debug is {'on' if debug else 'off'}", err=True)
-    click.echo(f"Color is {'on' if color else 'off'}", err=True)
+@click.option("--debug/--no-debug", default=False)
+@click.option("--color/--no-color", default=sys.stdout.isatty())
+@click.option("--quiet/--no-quiet", default=False)
+@click.version_option(version=__version__, prog_name="toot")
+def cli(debug: bool, color: bool, quiet: bool):
+    # click.echo(f"Debug is {'on' if debug else 'off'}", err=True)
+    # click.echo(f"Color is {'on' if color else 'off'}", err=True)
+    logging.basicConfig(level=logging.DEBUG)
 
 
 @cli.command()
 @click.argument("hostname")
-def instance(hostname: str, debug: bool):
+def instance(hostname: str):
     instance = asyncio.run(api.instance(hostname))
     print(instance.json())
 
@@ -53,11 +77,18 @@ def instance(hostname: str, debug: bool):
     help="Plain-text description of the media for accessibility purposes, one "
          "per attached media",
 )
+@click.option(
+    "-l",
+    "--language",
+    help="ISO 639-2 language code of the toot, to skip automatic detection",
+    callback=validate_language
+)
 def post(
     text: str,
     editor: str,
     media: Tuple[str, ...],
     description: Tuple[str, ...],
+    language: Optional[str],
 ):
     if editor and not sys.stdin.isatty():
         raise click.UsageError("Cannot run editor if not in tty.")
@@ -81,7 +112,7 @@ def post(
     media_ids = [m["id"] for m in uploaded_media]
 
     if uploaded_media and not text:
-        text = "\n".join(m['text_url'] for m in uploaded_media)
+        text = "\n".join(m["text_url"] for m in uploaded_media)
 
     if editor:
         text = editor_input(editor, text)
@@ -109,9 +140,6 @@ def post(
     # else:
     #     print_out("Toot posted: <green>{}</green>".format(response.get('url')))
 
-    # """Print FILENAME."""
-    # click.echo(filename)
-
 
 def _do_upload(file: str, description: Optional[str]):
     print("Faking upload:", file, description)
@@ -122,4 +150,4 @@ def _do_upload(file: str, description: Optional[str]):
 def main():
     # Allow overriding options using environment variables
     # https://click.palletsprojects.com/en/8.1.x/options/?highlight=auto_env#values-from-environment-variables
-    cli(auto_envvar_prefix='TOOT')
+    cli()
